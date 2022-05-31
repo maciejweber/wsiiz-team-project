ece5c4938a9a23f4fd5202aae262bf153acba672
Maciej Weber <maciejwebber@gmail.com>
2022-06-01 00:59:22 +0200

    likes & dislikes count

--- api/posts/serializers.py
+++ api/posts/serializers.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from rest_framework.fields import CurrentUserDefault
 
 from .models import Post
 from estates.models import Estate
@@ -15,15 +14,41 @@ class PostSerializer(serializers.ModelSerializer):
     content = serializers.CharField()
     estate = serializers.PrimaryKeyRelatedField(queryset=Estate.objects.all())
     author = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    likes = UserSerializer(read_only=True, many=True)
-    dislikes = UserSerializer(read_only=True, many=True)
+    likes_count = serializers.SerializerMethodField('get_likes_count')
+    dislikes_count = serializers.SerializerMethodField('get_dislikes_count')
+
+    def get_likes_count(self, obj):
+        return obj.likes.count()
+    def get_dislikes_count(self, obj):
+        return obj.dislikes.count()
 
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ['id','title','content','estate','author','created_on','likes_count','dislikes_count']
 
     def to_representation(self, instance):
         rep = super().to_representation(instance)
         rep['estate'] = EstateSerializer(instance.estate).data
         rep['author'] = UserSerializer(instance.author).data
         return rep
+
+
+class PostDetailSerializer(serializers.ModelSerializer):
+    title = serializers.CharField()
+    content = serializers.CharField()
+    estate = EstateSerializer()
+    author = UserSerializer() 
+    likes = UserSerializer(read_only=True, many=True)
+    dislikes = UserSerializer(read_only=True, many=True)
+    likes_count = serializers.SerializerMethodField('get_likes_count')
+    dislikes_count = serializers.SerializerMethodField('get_dislikes_count')
+
+    def get_likes_count(self, obj):
+        return obj.likes.count()
+    def get_dislikes_count(self, obj):
+        return obj.dislikes.count()
+
+
+    class Meta:
+        model = Post
+        fields = ['id','title','content','estate','author','likes','dislikes']
