25cba8e7584217c0f6a3e136b47b5b6b5cc03066
Maciej Weber <maciejwebber@gmail.com>
2022-06-10 21:25:12 +0200

    comments serializer

--- api/comments/serializers.py
+++ api/comments/serializers.py
@@ -8,17 +8,12 @@ from users.serializers import UserSerializer
 class CommentSerializer(serializers.ModelSerializer):
     text = serializers.CharField() 
     post = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all())
-    author = UserSerializer()
+    author = serializers.HiddenField(default=serializers.CurrentUserDefault())
 
     class Meta:
         model = Comment
         fields =['text', 'post', 'author']
         read_only_fields = ['author']
-
-    def to_representation(self, instance):
-        rep = super().to_representation(instance)
-        rep['author'] = UserSerializer(instance.author).data
-        return rep
 
 class CommentDetailSerializer(serializers.ModelSerializer):
     text = serializers.CharField() 
--- api/comments/views.py
+++ api/comments/views.py
@@ -4,20 +4,18 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import CreateAPIView
 
 from .serializers import CommentSerializer
 from .models import Comment
 from .permissions import IsCommentOwnerOrAdmin
 
-class CommentView(APIView):
+class CommentView(CreateAPIView):
     permission_classes = [IsAuthenticated]
+    serializer_class = CommentSerializer
 
-    def post(self, request, format=None):
-        serializer = CommentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(author=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        serializer.save()
 
 class CommentDetailView(APIView):
     permission_classes = [IsAuthenticated, IsCommentOwnerOrAdmin]
